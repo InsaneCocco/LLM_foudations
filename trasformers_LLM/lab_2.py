406295cabca0ec2e3bd2275ea10c5f9face13464
Juan Pablo Cocco Alcala <jpcocco20@gmail.com>
2025-07-02 07:51:16 -0600

    Text split as a preprocess for embeddings

--- trasformers_LLM/lab_2.py
+++ trasformers_LLM/lab_2.py
@@ -6,22 +6,7 @@ max_length = 100
 
 generation = pipeline('text-generation')
 
-prompt = ('''I want to analize the sentiment of phrases, for each phrase tag it as negative or positive. 
-Rewrite the sentence you are analizing and add the sentiment: 
-example prompts and responses:
-This new technology is groundbreaking and will revolutionize the industry.
-Sentiment: Positive
-Finding a solution to the problem was more difficult than I anticipated.
-Sentiment: Negative
-
-Do not add any other phrase. Just use what i give you.
-phrases to analize:
-I love spending time with my family and friends.
-Sentiment:
-The weather is terrible today, and everything went wrong
-Sentiment:
-The concert last night was amazing! The performers were outstanding
-Sentiment:
+prompt = ('''You are a translator
 ''')
 
 output = generation(
